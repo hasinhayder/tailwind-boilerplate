becfc02202c65c433b86b5368c5486038cf951b2
Hasin Hayder <hasin@leevio.com>
2022-02-12 20:14:33 +0600

    Package Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-    // purge: {
-    //   enabled: true,
-    //   content:['./*.html']
-    // },
     purge: ['./*.html'],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
